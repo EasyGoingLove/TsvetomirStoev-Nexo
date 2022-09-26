d0edc04c00e06e07e2cfcef40ab76d5d70d9d38e
Tsvetomir Stoev <tsvetomirstoev8@gmail.com>
2022-09-26 15:44:13 +0300

    NE-1 Successful connection with MetaMaks and CoinBase wallets

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal';
+import {CoinbaseWalletSDK} from '@coinbase/wallet-sdk';
+
+const providerOptions = {
+    coinbasewallet:{
+      package: CoinbaseWalletSDK,
+      options:{
+        appName : "Nexify"
+      }
+    },
+}
 
 function App() {
+
+  async function connectWallet() {
+    try {
+      
+      let web3Modal = new Web3Modal({
+        cacheProvider: false,
+        providerOptions,
+        theme : {
+          background:'red',
+          main:'blue',
+          secondary:'green',
+          hover:'yellow',
+          border:'white'
+        }
+      });
+      const web3ModalInstance = await web3Modal.connect();
+      const web3ModalProvider = new ethers.providers.Web3Provider(web3ModalInstance);
+      console.log(web3ModalProvider);
+
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={connectWallet}>Open Sesamesz</button>
       </header>
     </div>
   );
